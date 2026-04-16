7f640b5f297ef5b43de3d623e893813d8d50b24d
Ton Nom <ton@email.com>
2026-04-17 01:24:56 +0200

    change conf

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,11 +1,9 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchPage = async () => {
-  try {
-    const res = await fetch(`${API_URL}/api/pages?populate=*`);
-    return await res.json();
-  } catch (error) {
-    console.error("API error:", error);
-    return null;
-  }
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/pages?populate[sections][populate]=*`
+  );
+
+  return res.json();
 };
