5fbbdd52ceffae04dc2a3957bb47bf68edc29e9e
Ton Nom <ton@email.com>
2026-04-16 17:49:11 +0200

    change img

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 import useReveal from "../../hooks/useReveal";
-import heroImage from "../../assets/bg_hero.webp"; // 🔥 compressée !
+import heroImage from "../../assets/bg_hero.jpg"; // 🔥 compressée !
 import logo from "../../assets/logo_white.png";
 
 export default function Hero() {
