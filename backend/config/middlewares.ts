43e6c09e671a339b211b66615b337f8247469c53
Ton Nom <ton@email.com>
2026-04-17 12:35:37 +0200

    change middle wares

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,16 +1,22 @@
-import type { Core } from '@strapi/strapi';
-
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://palto-ten.vercel.app',
+        'http://localhost:5173'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      headers: '*',
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
-
-export default config;
